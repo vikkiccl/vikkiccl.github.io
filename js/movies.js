54100d5eecc5ee4a111190b2a3f9ad196f35e213
vikkiccl <122484082+vikkiccl@users.noreply.github.com>
2023-02-09 03:35:28 +0800

    add js

---
+++ js/movies.js
@@ -0,0 +1,64 @@
+function getMovies() {
+    return [
+        {
+            id:1,
+            type:"now",
+            thumbnail:"../images/movie/nature.jpg",
+            src:"../videos/animation/1A.mp4",
+            name:"Nature",
+            cast:"Nagasaki Kaho, Akae Miyu",
+            director:"Lau Lok",
+            duration: 136
+        },
+        {
+            id:2,
+            type:"now",
+            thumbnail:"../images/movie/wildlife.jpg",
+            src:"../videos/animation/1B.mp4",
+            name:"Wildlife",
+            cast:"Anne Shinohara, Uesawa Yura",
+            director:"Pottinger Harbin",
+            duration: 131
+        },
+        {
+            id:3,
+            type:"now",
+            thumbnail:"../images/movie/wonders.jpg",
+            src:"../videos/animation/1C.mp4",
+            name:"Wonders",
+            cast:"Jurky Vhew, Parker Eatton",
+            director:"Gabs Oller",
+            duration: 150
+        },
+        {
+            id:4,
+            type:"upcoming",
+            thumbnail:"../images/movie/casablanca.png",
+            src:"../videos/animation/3B.mp4",
+            name:"Casablanca",
+            cast:"Humphrey Bogart, Ingrid Bergmen",
+            director:"Michael Curtiz",
+            duration: 130
+        },
+        {
+            id:5,
+            type:"upcoming",
+            thumbnail:"../images/movie/bigbuck.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.mp4",
+            name:"Bigbuck",
+            cast:"Rachel Hummer, Macy Sepher",
+            director:"Heidi Sensei",
+            duration: 142
+        },
+        {
+            id:6,
+            type:"upcoming",
+            thumbnail:"../images/movie/chocolate.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
+            name:"Chocolate",
+            cast:"Harimoto Rin, Mende Miwa",
+            director:"Abyell Team",
+            duration: 127
+        }
+    ]
+}
